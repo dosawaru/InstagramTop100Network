a08829241bf8f9b328f004274c5e109e924060eb
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2025-04-14 15:29:56 -0700

    Implement data loading and include node and link creation

--- dosawaru.js
+++ dosawaru.js
@@ -6,5 +6,33 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Loaded Top_Influencers.csv file");
     influencers_data = values[0];
     console.log(influencers_data);
+
+    // Get window dimensions
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    // Create svg and tooltip
+    const svg = d3.select("svg").attr("width", width).attr("height", height);
+    const tooltip = d3.select(".tooltip");
+
+    // Process data to nodes
+    const nodes = influencers_data.map((d) => ({
+      id: d.Rank,
+      name: d["Channel Info"],
+      followers: d.Followers,
+      category: d.Category,
+      likes: d["Avg. Likes"],
+      score: +d["Influence Score"],
+    }));
+
+    console.log(nodes);
+
+    // Process data to links
+    const links = nodes.slice(1).map((node, index) => ({
+      source: nodes[index].id,
+      target: node.id,
+    }));
+
+    console.log(links);
   });
 });
