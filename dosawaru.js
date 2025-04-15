fcbdc8e06469772e6c8449431bbbef06b47bc9ef
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2025-04-14 19:03:09 -0700

    Update JavaScript for better dynamic node sizing, adjustments to keep nodes within bounds and CSS responsiveness

--- dosawaru.js
+++ dosawaru.js
@@ -8,8 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(influencers_data);
 
     // Get window dimensions
+    const headerHeight = document.querySelector("#header").offsetHeight;
     const width = window.innerWidth;
-    const height = window.innerHeight;
+    const padding = 20;
+    const height = window.innerHeight - headerHeight - padding;
 
     // Create svg and tooltip
     const svg = d3.select("svg").attr("width", width).attr("height", height);
@@ -35,6 +37,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     console.log(links);
 
+    const radiusScale = d3
+      .scalePow()
+      .exponent(8)
+      .domain([70, d3.max(nodes, (d) => d.score)])
+      .range([5, 65]);
+
     // Create simulation
     const simulation = d3
       .forceSimulation(nodes)
@@ -44,9 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
           .forceLink()
           .id((d) => d.id)
           .links(links)
-          .distance(100)
+          .distance(200)
       )
-      .force("charge", d3.forceManyBody().strength(-50))
+      .force("charge", d3.forceManyBody().strength(-70))
       .force("center", d3.forceCenter(width / 2, height / 2));
 
     // Draw links
@@ -56,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .enter()
       .append("line")
       .attr("class", "links")
-      .attr("stroke-width", 2)
+      .attr("stroke-width", 1)
       .attr("stroke", "#999");
 
     // Draw nodes
@@ -72,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .append("clipPath")
       .attr("id", (d) => `clip-${d.id}`)
       .append("circle")
-      .attr("r", (d) => (d.score * 4.5) / 8)
+      .attr("r", (d) => radiusScale(d.score))
       .attr("cx", 0)
       .attr("cy", 0);
 
@@ -83,20 +91,32 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(`profile_pics/${d.name}.jpg`);
         return `profile_pics/${d.name}.jpg`;
       })
-      .attr("width", (d) => d.score * 1.5)
-      .attr("height", (d) => d.score * 1.5)
-      .attr("x", (d) => -(d.score * 1.5) / 2)
-      .attr("y", (d) => -(d.score * 1.5) / 2)
-      .attr("clip-path", (d) => `url(#clip-${d.id})`);
+      .attr("width", (d) => radiusScale(d.score) * 2)
+      .attr("height", (d) => radiusScale(d.score) * 2)
+      .attr("x", (d) => -radiusScale(d.score))
+      .attr("y", (d) => -radiusScale(d.score))
+      .attr("clip-path", (d) => `url(#clip-${d.id})`)
+      .style("border", "1px solid white");
 
+    // Dynamically update the positions of the nodes/links and keep nodes within the window bounds
     simulation.on("tick", () => {
+      node.attr("transform", (d) => {
+        d.x = Math.max(
+          padding + radiusScale(d.score) / 2,
+          Math.min(width - radiusScale(d.score), d.x)
+        );
+        d.y = Math.max(
+          headerHeight / 2 + radiusScale(d.score) / 2,
+          Math.min(height - radiusScale(d.score), d.y)
+        );
+        return `translate(${d.x},${d.y})`;
+      });
+
       link
         .attr("x1", (d) => d.source.x)
         .attr("y1", (d) => d.source.y)
         .attr("x2", (d) => d.target.x)
         .attr("y2", (d) => d.target.y);
-
-      node.attr("transform", (d) => `translate(${d.x},${d.y})`);
     });
   });
 });
